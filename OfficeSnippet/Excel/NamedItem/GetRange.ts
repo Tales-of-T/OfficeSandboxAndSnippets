a506f490c0507b6810707bc38655b79883abd357
Toby Baratta <t-tobara@microsoft.com>
2016-08-18 10:38:37 -0700

    Added some chart features & named collection setups. NamedCollection examples are really wonked up. I fixed as best I could.

---
+++ OfficeSnippet/Excel/NamedItem/GetRange.ts
@@ -0,0 +1,36 @@
+module CodeSnippets {
+    export function snippet_NamedItem_GetRange(): IInternalSnippet {
+        return {
+            name: "",
+            category: "",
+            showInAPIPlayground: true,
+            
+            setup: function () {
+                // TODO: Documentation folk, please either fill this in (if needed), or remove!
+                return Promise.resolve();
+            },
+            code: {
+                jsOrTs: function () {
+                    return Excel.run(function (ctx) {
+						var names = ctx.workbook.names;
+						var range = names.getItem('MyRange').getRange();
+						range.load('address');
+						return ctx.sync().then(function () {
+							console.log(range.address);
+						});
+					}).catch(function (error) {
+						console.log("Error: " + error);
+						if (error instanceof OfficeExtension.Error) {
+							console.log("Debug info: " + JSON.stringify(error.debugInfo));
+						}
+					});
+                }
+            },
+            validator: function () {
+                // TODO: Documentation folk, this test will FAIL until you fill in the appropriate validation!
+                return Promise.reject(new Error("Validation not defined, test bound to fail!"));
+            }
+        }
+    }
+    
+}
---
+++ OfficeSnippet/Excel/NamedItem/Getter.ts
@@ -0,0 +1,36 @@
+module CodeSnippets {
+    export function snippet_NamedItem_Getter(): IInternalSnippet {
+        return {
+            name: "",
+            category: "",
+            showInAPIPlayground: true,
+            
+            setup: function () {
+                // TODO: Documentation folk, please either fill this in (if needed), or remove!
+                return Promise.resolve();
+            },
+            code: {
+                jsOrTs: function () {
+                    return Excel.run(function (ctx) {
+						var names = ctx.workbook.names;
+						var namedItem = names.getItem('MyRange');
+						namedItem.load('type');
+						return ctx.sync().then(function () {
+							console.log(namedItem.type);
+						});
+					}).catch(function (error) {
+						console.log("Error: " + error);
+						if (error instanceof OfficeExtension.Error) {
+							console.log("Debug info: " + JSON.stringify(error.debugInfo));
+						}
+					});
+                }
+            },
+            validator: function () {
+                // TODO: Documentation folk, this test will FAIL until you fill in the appropriate validation!
+                return Promise.reject(new Error("Validation not defined, test bound to fail!"));
+            }
+        }
+    }
+    
+}
